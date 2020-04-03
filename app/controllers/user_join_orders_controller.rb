3ff646dc3cef9983c17118a5cb08da75e1d15a62
basmmohamed <basmasalah2017@gmail.com>
2020-04-03 19:00:25 +0200

    orderitem page linked to view invited and view join users and can  cancel user

--- app/controllers/user_join_orders_controller.rb
+++ app/controllers/user_join_orders_controller.rb
@@ -1,21 +1,26 @@
 class UserJoinOrdersController < ApplicationController
   def index
     @orders = Order.find(2)
-    @orderItems = OrderItem.where(order_id:@orders.id)
-    p @orderItems
     @user_joined_order = UserJoinOrder.where(order_id:@orders.id)
-    p @user_joined_order 
+    p @user_joined_order
   end
 
   def create
   end
 
   def destroy
+    p "params values: #{params}"
+
+    @user_joined_order = UserJoinOrder.find(params[:id])
+    @user_joined_order.destroy
+    redirect_to :order_items
+
   end
 
   def new
   end
 
   def show
+   
   end
 end
---
+++ app/helpers/user_invited_to_orders_helper.rb
@@ -0,0 +1,2 @@
+module UserInvitedToOrdersHelper
+end

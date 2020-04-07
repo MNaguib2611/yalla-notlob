46fa2b6089c10aea1ecef50bbd7d7c4971f93fde
Rehab Ayman <rehabayman.fcis@gmail.com>
2020-04-07 17:31:24 +0200

    Fixed Invitations Bug

--- app/controllers/user_invitations_controller.rb
+++ app/controllers/user_invitations_controller.rb
@@ -1,3 +1,24 @@
+class UserInvitationsController < ApplicationController
+
+    def update
+        @invitation = UserInvitedToOrder.find(params[:invitation])
+        @notice = ''
+
+        if params[:user_action] == 'accept'
+            @invitation.status = 'accepted'
+            @notice = 'You Accepted the Invitation'
+            UserJoinOrder.create order_id: @invitation.order_id, user_id: @invitation.guest_id
+            @order = Order.find(@invitation.order_id)
+            if @order.joined_num 
+                @order.joined_num = @order.joined_num + 1
+            else
+                @order.joined_num = 1
+            end
+            @order.save
+        else
+            @invitation.status = 'rejected'
+            @notice = 'You Rejected the Invitation'
+        end
         @invitation.save
         respond_to do |format|
             format.html { redirect_to notifications_path, notice: @notice, invitation_status: @invitation.status }

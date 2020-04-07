fa79cd4fecc4874b2f9bc251589e17393bc88771
Rehab Ayman <rehabayman.fcis@gmail.com>
2020-04-07 18:38:33 +0200

    Removed User_Notification

--- app/models/user_notification.rb
+++
@@ -1,5 +0,0 @@
-class UserNotification < ApplicationRecord
-    belongs_to :user
-    scope :unread_notifications, -> {where(status: "unread")}
-    scope :read_notifications, -> {where(status: "read")}
-end
